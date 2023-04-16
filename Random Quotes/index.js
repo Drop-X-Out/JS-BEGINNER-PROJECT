db7adafd54f99269bc6ecd8d7a7166f215be37d2
Drop X Out <123237107+Drop-X-Out@users.noreply.github.com>
2023-04-16 23:04:51 +0530

    Add files via upload

---
+++ Random Quotes/index.js
@@ -0,0 +1,12 @@
+const btn= document.getElementById("btn");
+const output = document.querySelector(".output");
+
+let quote = [
+   " Avoid daydreaming about the years to come.","You are the most important person in your whole life.","Always be true to who you are, and ignore what other people have to say about you.","Always be true to who you are, and ignore what other people have to say about you.","Only demonstrate your strength when it’s really required.","Subscribe to Drop X Out"
+]
+
+btn.addEventListener("click",()=>{
+    console.log("clicked");
+    let random = Math.floor(Math.random()*quote.length);
+    output.textContent = quote[random];
+})
